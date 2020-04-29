a9ed411908c638dd2a53aea05967d6a400dd72ef
Hansolo <thiemo.heims@icloud.com>
2020-04-29 22:10:34 +0800

    Added static base web page
    based on bootstrap and https://bootswatch.com/cyborg/ theme

--- app/web/urls.py
+++ app/web/urls.py
@@ -13,12 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
+from .views import *
+
+app_name = 'web'
 
 urlpatterns = [
-    path('', include('web.urls', namespace='web')),
-    path('admin/', admin.site.urls),
-    path('api/auth/', include('users.urls', namespace='users')),
-    path('api/api/', include('api.healthcheck.urls', namespace='api')),
+    path('', home, name='homepage'),
 ]
