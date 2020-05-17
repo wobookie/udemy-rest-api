bc22e9ca3accc7e1f1b72d0c2f0b39bc58092cea
hansolo <thiemo.heims@icloud.com>
2020-05-17 19:54:44 +0800

    startserver.sh

--- naupy/bin/startserver.sh
+++ naupy/bin/startserver.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
+echo "prepare application starting ..."
+export PYTHONPATH=$DJANGO_HOME:$PYTHONPATH
 
-echo "starting gunicorn with $DJANGO_WSGI_MODULE:application ... "
+echo "check db is ready... "
+python3 $DJANGO_HOME/manage.py wait_for_db
 
-gunicorn $DJANGO_WSGI_MODULE:application --workers=3 --bind $APP_ADRPORT --chdir $DJANGO_HOME
+echo "starting nginx server ... "
+nginx
+
+echo "starting gunicorn with $DJANGO_WSGI_MODULE:application in $DJANGO_HOME... "
+gunicorn $DJANGO_WSGI_MODULE:application --workers=3 --bind $DJANGO_ADRPORT
+# gunicorn $DJANGO_WSGI_MODULE:application --workers=3 --bind $DJANGO_ADRPORT --pythonpath $DJANGO_HOME
