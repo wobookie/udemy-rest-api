87aadbc22714e6cf4fce05baeabd58de11df8bd9
hansolo <thiemo.heims@icloud.com>
2020-05-17 19:22:12 +0800

    Revert "Setup Nginx, Gunicorn"
    This reverts commit 20025c9fd83df5129f1e87c6cebfb5466b17ac69.

--- naupy/bin/startserver.sh
+++ naupy/bin/startserver.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
-echo "prepare application starting ..."
-export PYTHONPATH=$DJANGO_HOME:$PYTHONPATH
 
-# django-admin collectstatic
-echo "check db is ready... "
-python3 $DJANGO_HOME/manage.py wait_for_db
+echo "starting gunicorn with $DJANGO_WSGI_MODULE:application ... "
 
-echo "starting nginx server ... "
-nginx
-
-echo "starting gunicorn with $DJANGO_WSGI_MODULE:application in $DJANGO_HOME... "
-gunicorn $DJANGO_WSGI_MODULE:application --workers=3 --bind $DJANGO_ADRPORT
-# gunicorn $DJANGO_WSGI_MODULE:application --workers=3 --bind $DJANGO_ADRPORT --pythonpath $DJANGO_HOME
+gunicorn $DJANGO_WSGI_MODULE:application --workers=3 --bind $APP_ADRPORT --chdir $DJANGO_HOME
